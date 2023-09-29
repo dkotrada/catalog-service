1c1af4ff3f4e8119fbde80ce116246107f662a37
dkotrada <dk@off217.de>
2023-09-29 17:20:06 +0300

    finish contract with service and repository

---
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookService.java
@@ -0,0 +1,49 @@
+package com.polarbookshop.catalogservice.domain;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookrepository) {
+        this.bookRepository = bookrepository;
+    }
+
+    public Iterable<Book> viewBookList() {
+        return bookRepository.findAll();
+    }
+
+    public Book viewBookDetails(String isbn) {
+        return bookRepository.findByIsbn(isbn)
+                .orElseThrow(() -> new BookNotFoundException(isbn));
+    }
+
+    public Book addBookToCatalog(Book book) {
+        if (bookRepository.existsByIsbn(book.isbn())) {
+            throw new BookAlreadyExistsException(book.isbn());
+        }
+        return bookRepository.save(book);
+    }
+
+    public void removeBookFromCatalog(String isbn) {
+        bookRepository.deleteByIsbn(isbn);
+    }
+
+    public Book editBookDetails(String isbn, Book book) {
+        return bookRepository.findByIsbn(isbn)
+                .map(existingBook -> {
+                    // When editing the book, all the Book fields can be updated except the ISBN code,
+                    // because it’s the entity identifier.
+                    var bookToUpdate = new Book(
+                            existingBook.isbn(),
+                            book.title(),
+                            book.author(),
+                            book.price()
+                    );
+                    return bookRepository.save(bookToUpdate);
+                })
+                // When changing the details for a book not in the catalog yet, create a new book.
+                .orElseGet(() -> addBookToCatalog(book));
+    }
+}
