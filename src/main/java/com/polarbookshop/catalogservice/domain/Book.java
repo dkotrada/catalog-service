d5512a57745638c7a0b17d212bfa9b70a79f883d
dkotrada <dymmy@mail.com>
2024-08-14 16:25:52 +0200

    Enable auditing for persistent entities
    indicate class as spring configuration

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -4,12 +4,23 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Version;
+
+import java.time.Instant;
 
 public record Book (
         @Id
         Long id,
+
+        @CreatedDate
+        Instant createdDate,
+
+        @LastModifiedDate
+        Instant lastModifiedDate,
+
         @NotBlank(message = "The book ISBN must be defined.")
         @Pattern(
                 regexp = "^([0-9]{10}|[0-9]{13})$",
