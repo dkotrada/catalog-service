e1d2a9221cd5e342f5a3a237f7236b9d881c1709
dkotrada <dymmy@mail.com>
2024-08-14 16:59:07 +0200

    Start with empty database and save all books with one line

--- src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
+++ src/main/java/com/polarbookshop/catalogservice/demo/BookDataLoader.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @Profile("testdata")
 public class BookDataLoader {
@@ -20,9 +22,9 @@ public class BookDataLoader {
 
     @EventListener(ApplicationReadyEvent.class)
     public void loadBookTestData() {
+        bookRepository.deleteAll();
         Book bookOne = Book.of("1234567891", "Northern Lights", "Lyra Silverstar", 9.90);
         Book bookTwo = Book.of("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
-        bookRepository.save(bookOne);
-        bookRepository.save(bookTwo);
+        bookRepository.saveAll(List.of(bookOne, bookTwo));
     }
 }
